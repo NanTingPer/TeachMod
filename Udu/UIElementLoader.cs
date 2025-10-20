db17ec1bee46950188904f21a49cb3cd9f63242e
Nan_Ting <a3380991070@163.com>
2025-10-20 15:50:31 +0800

    UIElement : 更改Update能响应被覆盖的顶级Element 添加实用类型-IfMaxSubElseAdd
    还需要修改 ActiveFalse和 ActiveTrue

--- Udu/UIElementLoader.cs
+++ Udu/UIElementLoader.cs
@@ -3,41 +3,38 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using Terraria;
 using Terraria.Graphics.Capture;
 using Terraria.ModLoader;
-using ParentUIElement = TeachMod.Udu.UIElement;
 
 namespace TeachMod.Udu;
 
-//1. 将事件只对ActiveUIElement生效 迁移到每个UIElement拥有一个Active
-//  1. 在UIElement添加一个字段 --> currentActive
-//  2. UIElementLoader遍历每个(没有父元素 && Active) 的UIElement，并进行Draw
+// 在触发事件时 遍历当前UI列表 为顶层UI触发
+// 如果是单击低层UI 则将低层UI置于顶层
 
 public static class UIElementLoader
 {
+    /// <summary>
+    /// 放置层级修改时 Draw的foreach报错，采用队列
+    /// <para> 锁: <see cref="_elementIndexEditLock"/> </para>
+    /// </summary>
+    private readonly static Queue<Action> _elementIndexEdit = [];
+    /// <summary>
+    /// <see cref="_elementIndexEdit"/> 队列添加与消耗的锁
+    /// </summary>
+    private readonly static object _elementIndexEditLock = new();
     /// <summary>
     /// 当前活跃的 UIElement
     /// </summary>
     private static UIElement _currentActive = null;
 
     /// <summary>
-    /// 每个顶级父元素当前活跃的UIElement 其中Key是顶级父元素
-    /// </summary>
-    private readonly static Dictionary<ParentUIElement, UIElement> _currentActives = [];
-
-    /// <summary>
     /// 历史活跃的UIElement
     /// </summary>
     private readonly static List<UIElement> _oldElement = [];
-
-    /// <summary>
-    /// 每个顶级父元素的历史活跃UIElement 其中Key是顶级父元素
-    /// </summary>
-    private readonly static Dictionary<ParentUIElement, UIElement> _oldElements = [];
-    
     private static SpriteBatch _spriteBatch;
     /// <summary>
     /// 鼠标悬浮触发
@@ -55,12 +52,35 @@ public static class UIElementLoader
     private static void DoUpdateHook(DoUpate orig, Main main, ref GameTime gameTime)
     {
         orig.Invoke(main, ref gameTime);
-        if(_currentActive?.IsMouseHover() ?? false) {
-            _currentActive.InvokMouseHover();
-            if(Main.mouseLeft && Main.mouseLeftRelease) {
-                _currentActive.InvokMouseClick();
+        //遍历全部UI 为顶层UI发送事件
+        for (int i = _elements.Count - 1; i >= 0; i--) {
+            var forelement = _elements[i];
+            if (forelement.active == false || forelement.IsMouseHover() == false)
+                continue;
+            forelement.InvokMouseHover();
+            //没按下鼠标就不管了
+            if (!Main.mouseLeft && !Main.mouseLeftRelease) {
+                forelement.InvokMouseClick();
+                //拉到顶
+                lock (_elementIndexEditLock) {
+                    try {
+                        _elementIndexEdit.Enqueue(() => {
+                            _elements.Remove(forelement);
+                            _elements.Add(forelement);
+                        });
+                    } finally { }
+                }
             }
+            break;
         }
+
+        //2
+        //if (_currentActive?.IsMouseHover() ?? false) {
+        //    _currentActive.InvokMouseHover();
+        //    if(Main.mouseLeft && Main.mouseLeftRelease) {
+        //        _currentActive.InvokMouseClick();
+        //    }
+        //}
     }
 
     private delegate void DrawCapture(Main main, Microsoft.Xna.Framework.Rectangle area, CaptureSettings settings);
@@ -76,11 +96,21 @@ public static class UIElementLoader
     private static void DoDrawHook(DoDraw orig, Main main, GameTime gametime)
     {
         orig.Invoke(main, gametime);
+        //这里可能会报错，因为遍历的时候不能添加内容
         foreach (var uIelement in _elements) {
             if (uIelement.Parent != null || uIelement.active == false) continue;
             _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Opaque, SamplerState.PointClamp, DepthStencilState.Default, RasterizerState.CullNone, null, Main.UIScaleMatrix);
             uIelement.Draw(_spriteBatch);
             _spriteBatch.End();
+        }
+
+        //将元素移动到顶部
+        lock (_elementIndexEditLock) {
+            try {
+                while(_elementIndexEdit.TryDequeue(out var action)) {
+                    action.Invoke();
+                }
+            } finally { }
         }
     }
 
@@ -104,7 +134,7 @@ public static class UIElementLoader
     /// <param name="el"></param>
     public static void Region(UIElement el)
     {
-        _elements.Add(el);
+        _elementIndexEdit.Enqueue(() => _elements.Add(el));
     }
 
     /// <summary>
@@ -112,7 +142,8 @@ public static class UIElementLoader
     /// </summary>
     public static void Remove(UIElement el)
     {
-        _elements.Remove(el);
+        _elementIndexEdit.Enqueue(() => _elements.Remove(el));
+        
     }
 
     /// <summary>
---
+++ UtilityType/IfMaxSubElseAdd.cs
@@ -0,0 +1,47 @@
+﻿namespace TeachMod.UtilityType;
+
+/// <summary>
+/// 如果value达到Max那么就开始减小，如果value达到Min那么就开始增加
+/// </summary>
+/// <param name="value">数值</param>
+/// <param name="max">最大值</param>
+/// <param name="min">最小值</param>
+/// <param name="step">步频</param>
+public class IfMaxSubElseAdd(float value, float max, float min, float step = 0.1f)
+{
+    public float Step { get; set; } = step;
+    public float Max { get; set; } = max;
+    public float Min { get; set; } = min;
+    public float Value { get; set; } = value;
+    private Statu statu = Statu.Add;
+    public static implicit operator float(IfMaxSubElseAdd value) => value.Value;
+    public static explicit operator int(IfMaxSubElseAdd value) => (int)value.Value;
+    public static explicit operator long(IfMaxSubElseAdd value) => (long)value.Value;
+    public IfMaxSubElseAdd Run()
+    {
+        if (Value >= Max - Step) {
+            statu = Statu.Sub;
+        } else if (Value <= Min + Step) {
+            statu = Statu.Add;
+        }
+        switch(statu) {
+            case Statu.Add: 
+            {
+                Value += Step;
+                break;
+            }
+            case Statu.Sub: 
+            {
+                Value -= Step;
+                break;
+            }
+        }
+        return this;
+    }
+
+    private enum Statu
+    {
+        Add,
+        Sub
+    }
+}
