5c1db76d94a5934f1a8c240be39f2a36a00eb0e0
Nan_Ting <a3380991070@163.com>
2025-12-22 09:40:46 +0800

    TeachModUtils yield return

--- Teachs/TeachModUtils.cs
+++ Teachs/TeachModUtils.cs
@@ -11,33 +11,34 @@ public static partial class TeachModUtils
         where T : Entity
     {
         var enumerator = iter.GetEnumerator();
-        List<T> v = [];
+        //List<T> v = [];
         while (enumerator.MoveNext()) {
             if (func(enumerator.Current))
-                v.Add(enumerator.Current);
+                yield return enumerator.Current;//v.Add(enumerator.Current);
         }
-        return v;
+        //return v;
     }
 
     public static IEnumerable<TOut> Select<TIn, TOut>(this ActiveEntityIterator<TIn> iter, Func<TIn, TOut> func)
         where TIn : Entity
     {
         var enumerator = iter.GetEnumerator();
-        List<TOut> v = [];
+        //List<TOut> v = [];
         while (enumerator.MoveNext()) {
-            v.Add(func(enumerator.Current));
+            //v.Add(func(enumerator.Current));
+            yield return func(enumerator.Current);
         }
-        return v;
+        //return v;
     }
 
     public static IEnumerable<T> AsIEnumerable<T>(this ActiveEntityIterator<T> iter)
         where T : Entity
     {
         var enumerator = iter.GetEnumerator();
-        List<T> v = [];
+        //List<T> v = [];
         while (enumerator.MoveNext()) {
-            v.Add(enumerator.Current);
+            yield return enumerator.Current; //v.Add(enumerator.Current);
         }
-        return v;
+        //return v;
     }
 }
