257981b0c9ea3fe9de343d797005d0095827a8ea
Nan_Ting <a3380991070@163.com>
2025-12-22 09:47:56 +0800

    Revert "TeachModUtils yield return"
    This reverts commit 5c1db76d94a5934f1a8c240be39f2a36a00eb0e0.
    
    已经忘了为什么会返回list了

---
+++ MuchBossBar.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using System.Linq;
+using TeachMod.Teachs;
+using Terraria;
+
+namespace TeachMod;
+
+public class MuchBossBar : TeachModSystem
+{
+    public override void PostUpdate(Main mian, ref GameTime gameTime)
+    {
+        if(Main.player.Length == 0)
+            return;
+
+        var boss = Main.ActiveNPCs.Where(n => n.boss).ToList();
+        var bosslife = boss.Select(b => b.life).ToList();
+    }
+}
